096b13c5fb3560064ab41e28fdd92bed39ca47a9
Yosefgid <jossykasshun@yahoo.com>
2026-03-24 16:14:52 +0000

    Feat: Green Red Testing Compeleted to avoid false positive

---
+++ TDD-Communication.Tests/UnitTest4.cs
@@ -0,0 +1,43 @@
+﻿namespace TDD_Communication.Tests;
+using Shouldly;
+using TDD.Communications;
+
+public class MoodTest
+{
+   
+    [Test]
+    public void CheckMood_WhereSingleLike_ReturnsLike()
+    {
+        var moodCheck = new Mood();
+        OutCome[] input = { OutCome.LIKE };
+        var result = moodCheck.EvaluateMoods(input);
+        result.ShouldBe(OutCome.LIKE);
+    }
+
+    [Test]
+    public void CheckMood_WhereSingleDislike_ReturnsDisLike()
+    {
+        var moodCheck = new Mood();
+        OutCome[] input = { OutCome.DISLIKE };
+        var result = moodCheck.EvaluateMoods(input);
+        result.ShouldBe(OutCome.DISLIKE);
+    }
+
+    [Test]
+    public void CheckMood_WhereDoubleLike_ReturnsNeutral()
+    {
+        var moodCheck = new Mood();
+        OutCome[] input = { OutCome.LIKE, OutCome.LIKE };
+        var result = moodCheck.EvaluateMoods(input);
+        result.ShouldBe(OutCome.NEUTRAL);
+    }
+
+    [Test]
+    public void CheckMood_WhereDoubleDisLike_ReturnsNeutral()
+    {
+        var moodCheck = new Mood();
+        OutCome[] input = { OutCome.DISLIKE, OutCome.DISLIKE };
+        var result = moodCheck.EvaluateMoods(input);
+        result.ShouldBe(OutCome.NEUTRAL);
+    }
+}
--- TDD.Communications/Mood.cs
+++ TDD.Communications/Mood.cs
@@ -4,10 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace TDD.Communications
 {
     public class Mood 
     {
+        public OutCome EvaluateMoods(OutCome[] inputs)
+        {
+            //int likeCount = inputs.Count(o => o == OutCome.LIKE);
+            //int dislikeCount = inputs.Count(o => o == OutCome.DISLIKE);
+
+            //if(dislikeCount == 0 && likeCount > 0)
+            return OutCome.NEUTRAL;
+        }
     }
 }
         
