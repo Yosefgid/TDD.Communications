e0b27bf9c7cf686cf7c4151d8657785df869521d
Yosefgid <jossykasshun@yahoo.com>
2026-03-24 12:15:20 +0000

    Feat: Add the first test method

--- TDD-Communication.Tests/UnitTest1.cs
+++ TDD-Communication.Tests/UnitTest1.cs
@@ -1,16 +1,23 @@
+using Shouldly;
+using TDD.Communications;
+
 namespace TDD_Communication.Tests
 {
-    public class Tests
+    public class CalculatorTests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
+  
         [Test]
-        public void Test1()
+        public void SumMiddleNumbers_ArrSingleElement_ReturnZero()
         {
-            Assert.Pass();
+            //Arrange
+            var calculator = new Calculator();
+            int[] numbers = { 1 };
+            //Act
+            var result = calculator.SumMiddleNumbers(numbers);
+            //Assert
+            result.ShouldBe(0);
+
+            //Assert.That(result, Is.EqualTo(0));
         }
     }
 }
