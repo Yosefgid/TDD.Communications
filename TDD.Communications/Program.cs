fec4841d0a28b1f2efb3ce6515c07614e39b78d8
Yosefgid <jossykasshun@yahoo.com>
2026-03-24 11:45:18 +0000

    Add project files.

---
+++ TDD.Communications/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TDD.Communications
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
