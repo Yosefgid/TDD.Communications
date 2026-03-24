839296ea139712e4d1479b6b79e4d185208b4b07
Yosefgid <jossykasshun@yahoo.com>
2026-03-24 13:07:09 +0000

    Add: add comments

--- TDD.Communications/Calculator.cs
+++ TDD.Communications/Calculator.cs
@@ -14,7 +14,13 @@ namespace TDD.Communications
             if (numbers.Length <= 2)
                 return 0;
             //Numbers is sorted by ascending order
-
+            //We are using orderBy method and using arrow/lambda function to sort the numbers 
+            // n refers to the current instance of a number from the numbers array
+            //n => n this is saying use this numberr to sort and will return n
+            //let us say numbers = {5, 2, 9}
+            //numbers[0] = num =5
+            //return 5 for 5 and 2 for 2 and 9 for 9 then toArray puts them in a new array
+            //and then orderby orders the numbers 
             var sorted = numbers.OrderBy(num => num).ToArray();
 
             var lowest = sorted[0];
@@ -29,7 +35,6 @@ namespace TDD.Communications
             }
 
             return sum - lowest - highest;
-
                
         }
     }
