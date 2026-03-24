5e10244d33282b4320450682679da980c4d4a377
Yosefgid <jossykasshun@yahoo.com>
2026-03-24 11:53:00 +0000

    Feat: Add Dependencies

---
+++ TDD-Communication.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TDD_Communication.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
