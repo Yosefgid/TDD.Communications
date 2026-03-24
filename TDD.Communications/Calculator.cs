7e87000a3e224f65e5e53e76e6c004a010df8559
Yosefgid <jossykasshun@yahoo.com>
2026-03-24 11:59:14 +0000

    Feat: Add a Class Calculator

---
+++ TDD.Communications/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD.Communications
+{
+    public class Calculator
+    {
+        public int SumMiddleNumbers(int[] numbers)
+        {
+            //for now
+            return 0;
+        }
+    }
+}
