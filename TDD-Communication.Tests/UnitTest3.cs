9cca7a0460f24443a08ec3b414aebc50a0ecbc46
Yosefgid <jossykasshun@yahoo.com>
2026-03-24 14:58:20 +0000

    Feat: Add A test for string toa char of a string to ASCII codes

---
+++ TDD-Communication.Tests/UnitTest3.cs
@@ -0,0 +1,18 @@
+﻿using Shouldly;
+using TDD.Communications;
+using TDD_Communication;
+
+namespace TDD_Communication.Tests;
+
+public class AddAsciiCodesTest
+{
+
+    [Test]
+    public void AddAsciiCodes_WithSingleChar_ReturnsAsciiValue()
+    {
+        var calculator = new AddAsciiCodes();
+        string input = "a";
+        var result = calculator.AddAscii_Codes(input);
+        result.ShouldBe(97);
+    }
+}
---
+++ TDD.Communications/AddAsciiCodes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD.Communications
+{
+    public class AddAsciiCodes
+    {
+        public int AddAscii_Codes(string input)
+        {
+
+            //casting a char into an int produces its ASCII values
+            return (int)input[0];
+        }
+    }
+}
