cb3369bb1baf2df7476f24cc7e026563b244a130
Yosefgid <jossykasshun@yahoo.com>
2026-03-24 14:03:27 +0000

    Feat: Add a class to test mean

---
+++ TDD-Communication.Tests/UnitTest2.cs
@@ -0,0 +1,16 @@
+﻿using Shouldly;
+using TDD.Communications;
+
+namespace TDD_Communication.Tests;
+
+public class CalculateMeanTest
+{
+    [Test]
+    public void CalculateMean_ArrSingleElement_ReturnSameNumber()
+    {
+        var calculator = new Calculate_Mean();
+        int[] num = { 2 };
+        var result = calculator.CalculateMean(num);
+        result.ShouldBe(2);
+    }
+}
---
+++ TDD.Communications/CalculateMean.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD.Communications
+{
+    public class Calculate_Mean
+        {
+        public double CalculateMean(int[] num)
+        {
+            return num[0];
+        }
+    }
+}
